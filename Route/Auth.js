5d2d6b996c6d957f536694781024103212aab68c
srik9585 <srik9585a@gmail.com>
2023-09-04 16:00:51 +0530

    Middleware updated

--- Route/Auth.js
+++ Route/Auth.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const User = require('../Model/UserModel');
 
-const bcryptjs = require("bcryptjs");
+const jsw = require("jsonwebtoken");
+
+const sha = require('crypto')
+
 const AuthRoute = express.Router();
 
 AuthRoute.post('/Peak-tree/UserSignUp',async (req,res)=>{
@@ -9,14 +13,19 @@ AuthRoute.post('/Peak-tree/UserSignUp',async (req,res)=>{
         const { name, email, password,username } = req.body;
     
         const existingUser = await User.findOne({ email });
-        const existingUserid = await User.findOne({ username });
-        if (existingUser||existingUserid) {
+
+        if (existingUser) {
             return res
                 .status(400)
                 .json({ msg: "User with same email already exists!" });
         }
-    
-        const hashedPassword = await bcryptjs.hash(password, 8);
+
+        if (!name || !username || !email|| !password) {
+            res.status(404).send(
+            "All field are manditory",
+            );
+        }
+        hashedPassword = sha.createHash("sha512").update(password).digest("hex")
     
         let user = new User({
             username,
@@ -31,8 +40,28 @@ AuthRoute.post('/Peak-tree/UserSignUp',async (req,res)=>{
     }
 });
 
-AuthRoute.get("/Peak-tree/UserSignIn", async(req,res)=>{
-    
-})
 
+AuthRoute.get('/Peak-tree/UserSignIn',async (req,res)=>{
+    try{
+        const {email,password} = req.body
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            res.status(404).send("Student Doesn\'t Exist")
+        }
+
+        const ismatch = sha.createHash("sha512").update(password).digest('hex');
+
+        if (ismatch !== user.password) {
+            return res.status(400).json("Password is incorrect" );
+        }
+
+        const token = jsw.sign({id:user._id},'passwordKey')
+        res.json({token,...user._doc})
+    }
+    catch(e){
+
+    }
+});
 module.exports = AuthRoute;
